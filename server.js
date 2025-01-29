7c37bfb17eb211ed1b2d1282c4a9ab7a35b9ab81
Glago Gideon Elorm <103961380+glaogideonelorm@users.noreply.github.com>
2025-01-29 21:32:59 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors()); 
+
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+  try {
+    
+    const response = {
+      email: "glagogideonelorm2006@gmail.com", 
+      current_datetime: new Date().toISOString(), 
+      github_url: "<https://github.com/glaogideonelorm/HNG>" 
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error("Error generating response:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(` Server running at: http://localhost:${PORT}`);
+});
